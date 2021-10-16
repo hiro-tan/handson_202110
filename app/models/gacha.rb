aa6986cedfa02fcdf322fd2b4a5a574d75ceb653
hirota_yohei <yohei.hirota@drecom.co.jp>
2021-10-17 03:36:38 +0900

    lottery contentable

--- app/models/gacha.rb
+++ app/models/gacha.rb
@@ -2,6 +2,16 @@ class Gacha < ApplicationRecord
   has_many :gacha_contents
 
   def lottery_contentable
-    return "Implement me!"
+    contents = self.gacha_contents
+    total_weights = contents.sum(:weight)
+    r = rand(total_weights)
+
+    contents.each do |content|
+      r -= content.weight
+
+      return content.contentable if r < 0
+    end
+
+    raise "out of range. total:#{total_weights}, rand:#{r}"
   end
 end
